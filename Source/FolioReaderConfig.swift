9bf85c71bcb3e5fe7da1b75ff5bc546b402ee2b2
Can Akkaya <ufukcanakkaya@gmail.com>
2025-11-05 19:48:20 +0300

    Revert "add highlight delegate to reader"
    This reverts commit 94ed94219b13483b7acc0dd8633e4bc594b5e2ec.

--- Source/FolioReaderConfig.swift
+++ Source/FolioReaderConfig.swift
@@ -224,7 +224,6 @@ open class FolioReaderConfig: NSObject {
     open var localizedShareTextQuote = NSLocalizedString("Share text quote", comment: "")
     open var localizedSave = NSLocalizedString("Save", comment: "")
     open var localizedHighlightNote = NSLocalizedString("Note", comment: "")
-    open var localizedRemoveHighlight = NSLocalizedString("Remove", comment: "")
 
     public convenience init(withIdentifier identifier: String) {
         self.init()
